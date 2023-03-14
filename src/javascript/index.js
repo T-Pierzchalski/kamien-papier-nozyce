910849b60c61d33e929eda01f6afe9faf3ab2b03
TrueDie3 <t.pierzchalski0@gmail.com>
2023-03-14 19:15:48 +0100

    update 14.03.2023
    Gra w końcu wyszła po za konsolę!
    
    Jeszcze brakuję zapisywania wyników w postaci puntków.

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,5 +1,17 @@
 const opcja = ["kamień", "papier", "nożyce"];
 
+const btnKamien = document.querySelector(".kamien");
+const btnPapier = document.querySelector(".papier");
+const btnNozyce = document.querySelector(".nozyce");
+const btnZagraj = document.querySelector(".play");
+const buttons = document.querySelectorAll("button");
+
+const gameText = document.querySelector(".game-text");
+const span = document.createElement("span");
+span.style.cssText = "color:red";
+const reset = document.createElement("button");
+reset.textContent = "reset";
+
 function getComputerChoice() {
 	function random() {
 		const randomize = Math.floor(Math.random() * 100);
@@ -13,39 +25,86 @@ function getComputerChoice() {
 	}
 	return random();
 }
+
+//events
+btnKamien.addEventListener("click", function (e) {
+	btnKamien.classList.toggle("active");
+	return opcja[0];
+});
+btnPapier.addEventListener("click", () => {
+	btnPapier.classList.toggle("active");
+	return opcja[1];
+});
+btnNozyce.addEventListener("click", () => {
+	btnNozyce.classList.toggle("active");
+	return opcja[2];
+});
+
 function getPlayerChoice() {
-	const select = prompt("Kamień papier nożyce");
-	return select.toLowerCase();
+	if (btnKamien.classList.contains("active")) {
+		return opcja[0];
+	} else if (btnPapier.classList.contains("active")) {
+		return opcja[1];
+	} else if (btnNozyce.classList.contains("active")) {
+		return opcja[2];
+	}
 }
+btnZagraj.addEventListener("click", function () {
+	btnZagraj.classList.add("active");
+	gameText.appendChild(span);
+});
+
+// main game function
 function playRound(playerSelection, computerSelection) {
 	playerSelection = getPlayerChoice();
 	computerSelection = getComputerChoice();
 	function gameRules() {
+		const kompwin = "wygrał komputer!";
+		const playerwin = "wygrałeś!";
 		if (playerSelection === computerSelection) {
-			return playerSelection + " = " + computerSelection + " = remis";
+			return (span.textContent =
+				playerSelection + " = " + computerSelection + " = remis");
 		} else if (playerSelection === "nożyce" && computerSelection === "kamień") {
-			return (
-				playerSelection + " < " + computerSelection + " = wygrał komputer!"
-			);
+			return (span.textContent =
+				playerSelection + " < " + computerSelection + " = " + kompwin);
 		} else if (playerSelection === "nożyce" && computerSelection === "papier") {
-			return playerSelection + " > " + computerSelection + " = wygrałeś!";
+			return (span.textContent =
+				playerSelection + " > " + computerSelection + " = " + playerwin);
 		} else if (playerSelection === "papier" && computerSelection === "kamień") {
-			return playerSelection + " > " + computerSelection + " = wygrałeś!";
+			return (span.textContent =
+				playerSelection + " > " + computerSelection + " = " + playerwin);
 		} else if (playerSelection === "kamień" && computerSelection === "papier") {
-			return (
-				playerSelection + " < " + computerSelection + " = wygrał komputer!"
-			);
+			return (span.textContent =
+				playerSelection + " < " + computerSelection + " = " + kompwin);
 		} else if (playerSelection === "kamień" && computerSelection === "nożyce") {
-			return playerSelection + " > " + computerSelection + " = wygrałeś!";
+			return playerSelection + " > " + computerSelection + " = " + playerwin;
 		} else {
-			return "błąd";
+			return (span.textContent = "błąd");
 		}
 	}
-	return gameRules();
+	return (span.textContent = gameRules());
 }
+
 function game() {
 	for (let i = 0; i < 5; i++) {
-		console.log(playRound());
+		span.innerHTML += playRound() + "<br>";
+		buttons.forEach(button => {
+			if (btnZagraj.classList.contains("active")) {
+				button.classList.remove("active");
+			}
+		});
+		break;
 	}
 }
-console.log(game());
+
+btnZagraj.addEventListener("click", game);
+btnZagraj.addEventListener("click", () => {
+	gameText.appendChild(reset);
+});
+
+// restart gry
+
+reset.addEventListener("click", () => {
+	gameText.innerHTML = "";
+	span.innerHTML = "";
+});
